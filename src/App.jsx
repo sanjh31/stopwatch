24271814f48234d9c9482a859fd65809aac0fc34
Sanjana Negi <sanjananegi137@gmail.com>
2024-05-22 14:27:37 +0530

    second-change

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Stopwatch from './Components/StopwatchComponent/Stopwatch'
-import DigitalWatch from './Components/StopwatchComponent/DigitalWatchComponent/DigitalWatch'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      < DigitalWatch/>
+
   < Stopwatch/>
 
     </>
